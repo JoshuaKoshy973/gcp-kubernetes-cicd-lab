a4b76e53052b5d6e4ea72e0e43a0e4f41018c0bb
Joshua <joshuakoshyschool@gmail.com>
2026-08-31 16:38:26 -0500

    Add containerized Python application

---
+++ app/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return jsonify(
+        message="Hello from GCP Kubernetes CI/CD Lab",
+        version="v1"
+    )
+
+@app.route("/health")
+def health():
+    return jsonify(status="healthy")
+
+@app.route("/version")
+def version():
+    return jsonify(version="v1")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
